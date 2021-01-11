d27553e5a4b0657fae0aa43dc16509edb5ebc68e
theophane73360 <77209653+theophane73360@users.noreply.github.com>
2021-01-11 10:52:02 +0100

    Update Description.js

--- src/Description.js
+++ src/Description.js
@@ -4,7 +4,24 @@ export const Description = () => {
   return (
     <div className="description">
       <h4>Description</h4>
-      <p>El modi consendis serum unt poribus, te nam, coritas volut latem isqui nulparc hillabori tempori atatis ne dempe necatem. Itatem fugiam, cor aboratus isit, que et, si offictate am vellandae. Ro eum dolendit endis millesequi nimus quia dolorectatia pro oditaspis exernat endio. Nam autemporem vellabores venim rae poribus dollautaspe inctiat iberit quiderumque velis vid quatiat ditiorum eat quo corem laborib erecus, te et exces aut es illuptas et quo volorit prore verae simin corio dicimpore maximperum quam aut doluptibus, corero te volutem osaecte eic tor sam, invenis imintiunt magnienihil ipienis apedips andenis eiur acernam, veribernatur a eum se pro to omnia veliaes mi, adit, que idellor aciliatiorum as magnis dus eat ut volut utecaecum aut lant, nimo earior modipic ipsusae es accuptat modipis sinulpa non nobis deniam, ipsant utem aut quia dolupta turempor mo optaepe nam dollupt asitati busdaepudae venem fuga. Onectem. Delicatus in repudae.</p>
+      <p>La lettre de janvier 2021
+
+"Chers visiteurs, 
+
+Nous sommes heureux de vous annoncer l'ouverture de MaxiBiB (bien que fonctionnelle vous êtes actuellement sur une version encore en développement. Nous faisons tout notre possible pour la rendre stable). 
+Si il nous fallait décrire MaxiBiB nous dirions que c'est, avant toutes désignations ou appellation, une sorte de supra-entité ... Supra non pas en termes de valeur ou qualitativement mais supra de façon spatiale et structurelle. Presque comme l'entité des entités. Supra donc car trouvant sa place au dessus de. En fait, son but et d'englober et joindre des pratiques atomisées. C'est un peu comme si nous cherchions à construire des ponts au sein d'un vaste archipel. Nous savons que c'est un terrain glissant tant il est facile de tomber dans une rapport colonisant et enclavant dans une entreprise qui se veut enveloppante d'un sujet ou d'un objet. Finalement il n'y a qu'une respiration et quelques intentions entre une main qui contient et une main qui serre. Si vous sentez la notre se contracter ou seulement sa peau vous effleurer dites le nous, nous débrancherons MaxiBiB ! 
+
+Si il est important pour nous de prendre le temps de reformuler et étayer ce rôle que nous endossons, c'est parce que nous savons à quel point il est aussi délicat que nécessaire pour ce dans quoi nous oeuvrons. Bien que la fragilité, voir la précarité, de notre économies fait la beauté de ce que nous produisons, nous sommes aujourd'hui convaincus de l'utilité de déployer en notre sein, des outils utiles et fonctionnels pour permettre d'avantage de réflexions collectives et amplifier l'action commune. MaxiBiB tente d'en être un et rejoint de ce fait une myriade d'autres. Leurs buts étant de palier à la difficulté d'exister en tant que collectifs/individualité éditoriaux/éditoriale indépendant/indépendante et d'être reconnu comme tel/telle. À présent que ce cadre est posé et qui nous l'espérons éloignera les malentendus, nous pouvons vous parler de concrètement ce qu'est MaxiBiB.
+
+MaxiBiB est une espace web auto-hébergée à Romainville regroupant 6 (pour le moments) bibliothèques : La Bibliothèque A L'AMER, la Bibliothèque Brigade de Solidarité Populaire, la Bibliothèque Editions Burn~Août, la Bibliothèque Lallart, la Bibliothèque Show, la Bibliothèque Tabledepresse.com.
+Ces bibliothèques constituent un fond de 247 ressources, principalement PDF, pouvant être téléchargées librement et dont leurs usages — allant d'un usage sans aucune restriction y compris à des fins commerciales à un usage limité — sont définis préalablement par les licences Créative Commons. Il faut considérer chacune de ces bibliothèques comme des espaces virtuels relatifs à des formes d'initiatives, individuelles ou collectives, en lien avec l'édition indépendante leur permettant de stocker et rendre accessible l'entièreté ou une partie de leur production éditoriale. 
+
+Les trois buts effectifs principaux sont de : 
+ Permettre une plus ample dissémination des ressources produite en ajoutant à leurs canaux de diffusion habituels celui de MaxiBiB. 
+Permettre leur référencement et celui de leur auteur sur internet en suivant un protocole d'indexation défini par MaxiBiB. 
+Permettre l'archivage, le stockage et la documentation d'un pan du paysage éditorial actif indépendant. 
+
+Le but général est de permettre la création d'un supra-collectif-éditorial calqué sur le système de la coopérative agricole mettant en commun moyens de production et canaux de diffusion. .</p>
     </div>
   )
 }
